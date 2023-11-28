069b33349c21ae50a47c2ad66c303bfa7054f1d9
Lucas Neno <lucasneno@gmail.com>
2023-11-28 10:48:57 +0000

    Added Google Ads snippets to index.html and Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,33 +7,30 @@ function Contact() {
     setFormState({ ...formState, [event.target.name]: event.target.value });
   };
 
-  // const submitHandler = (event) => {
-  //   event.preventDefault();
-  //   const config = {
-  //     SecureToken: '0783c68f-b982-448b-a800-93f38ba6d2a7',
-  //     To: 'cloudneno@gmail.com',
-  //     From: formState.email,
-  //     Subject: `${formState.name} contacted to you in PersonalGO`,
-  //     Body: formState.message
-  //   };
-  //   if (window.Email) {
-  //     window.Email.send(config)
-  //       .then(response => {
-  //         console.log(response);
-  //         alert('Attempted to send email');
-  //       })
-  //       .catch(error => {
-  //         console.error("Email error:", error);
-  //       });
-  //   }
-  // };
+  // Google Ads Conversion Tracking Function
+  const gtag_report_conversion = (url) => {
+    const callback = function () {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    };
+    window.gtag('event', 'conversion', {
+        'send_to': 'AW-11421282636/isFfCLe1p_kYEMzii8Yq',
+        'event_callback': callback
+    });
+    return false;
+  }
+
+  // Handle Form Submission
+  const submitHandler = (event) => {
+    // Call the conversion tracking function
+    gtag_report_conversion();
+  };
 
   return (
     <div>
-
       <div name='inscreva-se' className='w-full h-auto pt-[80px] bg-primaryBg flex justify-center items-center p-4'>
-        <form className='flex flex-col max-w-[600px] w-full' action="https://formsubmit.co/dab8a2a1894adcfc80c9711b2b6da895" method="POST">
-
+        <form className='flex flex-col max-w-[600px] w-full' onSubmit={submitHandler} action="https://formsubmit.co/dab8a2a1894adcfc80c9711b2b6da895" method="POST">
           <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-accent1 text-primaryText'>Inscreva-se!</p>
             <p className='text-secondaryText py-4'>Está curioso? Inscreva-se para participar dos testes de nosso protótipo através do formulário abaixo.</p>
